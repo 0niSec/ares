1cde7540b636a46584971f1142c6b52682653567
Nick <9609041+0niSec@users.noreply.github.com>
2024-11-26 15:06:53 -0500

    updated cli

--- lib/ares/cli.rb
+++ lib/ares/cli.rb
@@ -1,15 +1,18 @@
 require "thor"
+require "ronin/nmap"
 
 module Ares
   class CLI < Thor
     desc "scan [OPTIONS] TARGETS", "Run an Nmap scan"
+    option :connect_scan, :type => :boolean, :aliases => "-sT", :desc => "Perform a connect scan"
+    option :syn_scan, :type => :boolean, :aliases => "-sS", :desc => "Perform a SYN scan", default: true
     def scan(*targets)
-      nmap = Ronin::Nmap.new
+      result = Ronin::Nmap.scan do |nmap|
 
-      nmap.targets = targets
-      nmap.options = options
-
-      nmap.run
+        nmap.targets = targets
+        nmap.syn_scan = :syn_scan
+        puts options[:connect_scan]
+      end
     end
   end
 end
