0fa651bf408dc21525953ef99e6bb999f4de9e1a
Nick <9609041+0niSec@users.noreply.github.com>
2024-11-26 17:23:22 -0500

    updated cli

--- lib/ares/cli.rb
+++ lib/ares/cli.rb
@@ -7,12 +7,7 @@ module Ares
     option :connect_scan, :type => :boolean, :aliases => "-sT", :desc => "Perform a connect scan"
     option :syn_scan, :type => :boolean, :aliases => "-sS", :desc => "Perform a SYN scan", default: true
     def scan(*targets)
-      result = Ronin::Nmap.scan do |nmap|
-
-        nmap.targets = targets
-        nmap.syn_scan = :syn_scan
-        puts options[:connect_scan]
-      end
+      xml = Ronin::Nmap.scan(sudo: false, targets: targets)
     end
   end
 end
